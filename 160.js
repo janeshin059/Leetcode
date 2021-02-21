cea7997c145d5e5cf01de309848e2fddde1a70d3
janeshin059 <janeshin059@gmail.com>
2021-02-21 11:30:50 +0900

    Update 160.js

--- 160.js
+++ 160.js
@@ -21,3 +21,21 @@ var getIntersectionNode = function(headA, headB) {
   return p1;
   
 };
+//Hash table
+/*
+var getIntersectionNode = function(headA, headB) {
+    let l1 = headA;
+    let l2 = headB;
+    let hashMap = new Map();
+    while(l1){
+        hashMap.set(l1,true)
+        l1 = l1.next;
+    }
+    while(l2) {
+        if(hashMap.has(l2)){
+            return l2;
+        }
+        l2 = l2.next;
+    }
+    return;
+}; */
