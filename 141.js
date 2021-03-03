d19ded86a35a91ebcbca556a7832cdb8ddd04280
janeshin059 <janeshin059@gmail.com>
2021-03-03 12:32:07 +0900

    141 linked ilst cycle

---
+++ 141.js
@@ -0,0 +1,19 @@
+var hasCycle = function(head){
+  let fastPointer = head;
+  let slowPointer = head;
+
+  //while loop will continue until the fast pointer reaches the end of the linked list
+
+  while (fastPointer !== null && fastPointer.next !== null){
+    fastPointer = fastPointer.next.next;
+    slowPointer = slowPointer.next;
+    //if they were met, it means there is a cycle
+    if(fastPointer == slowPointer){
+      return true;
+    }
+  }
+  return false;
+}
+
+// time O(n) : traverse the linked list
+//space O(1) : didn't make an extra memory
--- file1.js
+++ file1.js
@@ -1,142 +1,16 @@
-class Node{
-  constructor(data, left = null,right = null){
-    this.data = data;
-    this.left = left;
-    this.right = right;
-  }
-}
-
-class BST {
-  constructor(){
-    this.root = null;
-  }
-  
-  add(data){
-    const node = this.root;
-    
-  
-
-   
-   
-  }
-
-  findMin() {
-    let current = this.root;
-    while(current.left){
-      current = current.left
-    }
-    return current.data;
-  }
-
-  findMax(){
-   let current = this.root;
-   while(current.right){
-     current = current.right;
-   }
-   return current.data;
-  }
-
-  find(data){
-    let current = this.root;
-    while(current.data !== data){
-      if(current.data > data){
-        current = current.left;
-      }
-      else if(current.data < data){
-        current = current.right;
-      }
-     if(current == null){
-       return null;
-     }
-    }
-    return current;
-  }
-
-  isPresent(data){
-    let current = this.root;
-    while(current){
-      if(data === current.data){
-      return true;
-    }
-    else if(data < current.data){
-      current = current.left;
-    }
-    else current = current.right;
-    }
-    return false;
-  }
-
-  remove(data){
-    
-  }
-
-  isBalanced(){
-    return (this.findMinHeight() >= this.findMaxHeight() -1);
-  }
-  findMinHeight(node = this.root){
-    if(node == null) return null;
-    let left = this.findMinHeight(node.left);
-    let right = this.findMinHeight(node.right);
-    if(left < right){
-      return left +=1;
-    }
-    else return right +=1;
-  }
-
-  findMaxHeight(node = this.root){
-    if(node == null) return null;
-    let left = this.findMinHeight(node.left);
-    let right = this.findMaxHeight(node.right);
-    if(left > right){
-      return left += 1;
-    }else return right +=1;
-  }
-
-  inOrder(){
-   
-  }
-
-  postOrder(){
-    
-
-  }
-
-  preOrder(){
-    if(this.root == null){
-      return null;
-    }else {
-      let result = new Array();
-      function traversePreOrder(node){ 
-        result.push(node.data);
-        node.left && traversePreOrder(node.left);
-        node.right && traversePreOrder(node.right);
-      }
-      traversePreOrder(this.root);
-      return result;
-    }
-
-   
-
-  }
-
-  inorderTraversal = function(root){
-    const output = [];
-    if(root === null){
-      return output;
-    }
-    const stack = [];
-    let curr = root;
-    while(curr !== null || stack.length !== 0){
-      if(curr !== null){
-        stack.push(curr);
-        curr = curr.left;
-      }
-      else {
-        curr = stack.pop();
-        output.push(curr.val);
-        curr = curr.right;
+var buddyStrings = function(A, B) {
+    let swap = {}
+    for(let i = 0; i<A.length; i++){
+      if(A[i] !== B[i]){
+        if(!swap.A){
+          swap.A = A[i];
+          swap.B = B[i];
+        }else {
+          if(swap.A !== B[i] || swap.B !== A[i]){
+            return false;
+          }
+        }
       }
     }
-    return output;
-  }
-}
+    return true;
+};
